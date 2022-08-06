b01ee92b7c4bf0d853c702a3176da2155dda5665
HackUnit <j.larose26@gmail.com>
2022-08-06 16:57:33 -0500

    Now adding app.py to build climate app

---
+++ app.py
@@ -0,0 +1,27 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+
+hello_list = ["Hello", "World!"]
+hello_dict = {"Hello": "World!"}
+
+@app.route("/")
+def home():
+    return "Hi"
+
+@app.route("/normal")
+def normal():
+    return str(hello_list)
+
+@app.route("/jsonified")
+def jsonified_list():
+    return jsonify(hello_list)
+
+@app.route("/dict")
+def dictionary():
+    return hello_dict
+
+def hello_world():
+    return 'Hello world'
