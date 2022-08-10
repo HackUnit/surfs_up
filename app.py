a12f623577d53cec1f6d2da5cd65327c1078090a
HackUnit <j.larose26@gmail.com>
2022-08-10 15:03:23 -0500

    Adding copy of app.py

--- app.py
+++ app.py
@@ -1,27 +1,52 @@
-from flask import Flask
+import datetime as dt
+
+import numpy as np
+
+import pandas as pd
+
+import sqlalchemy
+
+from sqlalchemy.ext.automap import automap_base
+
+from sqlalchemy.orm import Session
+
+from sqlalchemy import create_engine, func
+
+
+from flask import Flask, jsonify
+
+
+
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+Base = automap_base()
+
+Base.prepare(engine, reflect=True)
+
+Measurement = Base.classes.measurement
+
+Station = Base.classes.station
+
+session = Session(engine)
+
 
 app = Flask(__name__)
 
-@app.route('/')
-
-hello_list = ["Hello", "World!"]
-hello_dict = {"Hello": "World!"}
-
 @app.route("/")
-def home():
-    return "Hi"
 
-@app.route("/normal")
-def normal():
-    return str(hello_list)
+def welcome():
+    return(
+    '''
+    Welcome to the Climate Analysis API!
 
-@app.route("/jsonified")
-def jsonified_list():
-    return jsonify(hello_list)
+    Available Routes:
 
-@app.route("/dict")
-def dictionary():
-    return hello_dict
+    /api/v1.0/precipitation
 
-def hello_world():
-    return 'Hello world'
+    /api/v1.0/stations
+
+    /api/v1.0/tobs
+
+    /api/v1.0/temp/start/end
+    '''
+    )
